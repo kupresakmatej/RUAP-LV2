b22bdc931c8eef53e0275ea99b90523075033b52
kupresakmatej <matej.kupresak@gmail.com>
2023-11-10 18:04:47 +0100

    Add exercise 1

---
+++ ApiProject/ApiProject/Controllers/ContactController.cs
@@ -0,0 +1,28 @@
+﻿using ApiProject.Models;
+using ApiProject.Service;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContactController : ControllerBase
+    {
+        private ContactRepository contactRepository;
+
+        public ContactController()
+        {
+            this.contactRepository = new ContactRepository();
+        }
+
+        public Contact[] Get()
+        {
+            return contactRepository.GetAllContacts();
+        }
+    }
+}
